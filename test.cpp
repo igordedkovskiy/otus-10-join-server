75aba907fe4952a96e0d61870a8556b67ab9d611
Igor Dedkovskiy <divmy@yandex.ru>
2022-07-21 18:07:31 +0300

    Update tests

--- test.cpp
+++ test.cpp
@@ -115,38 +115,44 @@ TEST(TEST_ASYNC, test_lib)
         namespace fs = std::filesystem;
         const auto path{fs::absolute(".")};
         using iterator = fs::directory_iterator;
-        std::string fname;
+        std::vector<std::string> fnames;
         for(auto it{iterator(path)}; it != iterator(); ++it)
         {
             const auto path{fs::absolute(it->path())};
             if(!fs::is_directory(path))
             {
-                fname = path.filename().string();
+                const auto fname{path.filename().string()};
                 if(regex_match(fname, regexp_cmp))
-                    return fname;
+                    fnames.emplace_back(std::move(fname));
             }
         }
-        return fname;
+        return fnames;
     };
 
     const auto fname1{find_file("(bulk.*-1.log)")};
     ASSERT_FALSE(fname1.empty());
-    std::fstream file1{fname1, std::fstream::in};
-    std::stringstream out1;
     std::stringstream ref1{"bulk: cmd1, cmd2, cmd3\n"
                            "bulk: cmd4, cmd5\n"};
-    out1 << file1.rdbuf();
-    ASSERT_TRUE(out1.str() == ref1.str());
+    for(const auto& fname:fname1)
+    {
+        std::fstream file1{fname, std::fstream::in};
+        std::stringstream out1;
+        out1 << file1.rdbuf();
+        ASSERT_TRUE(out1.str() == ref1.str());
+    }
 
     const auto fname2{find_file("(bulk.*-2.log)")};
     ASSERT_FALSE(fname2.empty());
-    std::fstream file2{fname2, std::fstream::in};
-    std::stringstream out2;
     std::stringstream ref2{"bulk: cmd1, cmd2\n"
                            "bulk: cmd3, cmd4\n"
                            "bulk: cmd5, cmd6, cmd7, cmd8, cmd9\n"};
-    out2 << file2.rdbuf();
-    ASSERT_TRUE(out2.str() == ref2.str());
+    for(const auto& fname:fname2)
+    {
+        std::fstream file2{fname, std::fstream::in};
+        std::stringstream out2;
+        out2 << file2.rdbuf();
+        ASSERT_TRUE(out2.str() == ref2.str());
+    }
 }
 
 int main(int argc, char** argv)
