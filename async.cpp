e583661fdca9edc0a6441814541fbd8915efae2d
Igor Dedkovskiy <divmy@yandex.ru>
2022-07-22 16:17:23 +0300

    Uncouple... everything: unfinished

--- async.cpp
+++ async.cpp
@@ -5,7 +5,11 @@
 #include <fstream>
 #include <vector>
 #include <list>
+#include <queue>
 #include <unordered_map>
+#include <thread>
+#include <cstdint>
+#include <chrono>
 
 #include "async.hpp"
 #include "CmdCollector.hpp"
@@ -14,9 +18,62 @@
 namespace
 {
 
+struct block_t
+{
+    enum class InputType { STATIC, DYNAMIC };
+    InputType m_type{InputType::STATIC};
+    std::time_t m_block_time;
+    size_type m_block_id{0};
+    std::vector<std::string> m_block;
+};
+
+struct commands_queue_t
+{
+    void process_cmd(std::string &&cmd)
+    {
+        if(cmd == "{")
+        {
+            m_queue.back().m_type = block_t::InputType::DYNAMIC;
+            if(++m_braces == 1)
+            {
+                if(m_cmds.size() > 0)
+                    m_block_finished = true;
+            }
+        }
+        else if(cmd == "}")
+        {
+            if(m_braces == 0)
+                throw ParseErr::incorrect_format;
+            if(--m_braces == 0)
+            {
+                m_queue.back().m_type = block_t::InputType::STATIC;
+                m_block_finished = true;
+            }
+        }
+        else
+        {
+            m_cmds.emplace_back(std::move(cmd));
+            if(m_cmds.size() == 1)
+            {
+                m_queue.back().m_block_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+                ++m_queue.back().m_block_id;
+            }
+            if(m_braces == 0)
+            {
+                if(m_cmds.size() == m_capacity)
+                    m_block_finished = true;
+            }
+        }
+    }
+
+    enum class ParseErr: std::uint8_t { incorrect_format };
+    std::size_t m_braces{0};
+    std::queue<block_t> m_queue;
+};
+
 struct Handlers
 {
-    handler_t create(CmdCollector&& collector)
+    handler_t create(commands_queue_t&& collector)
     {
         m_last = !m_streams.empty() ? m_last + 1 : 1;
         m_streams.insert(std::make_pair(m_last, std::move(collector)));
@@ -37,7 +94,7 @@ struct Handlers
         return m_streams.size();
     }
 
-    std::unordered_map<handler_t, CmdCollector> m_streams;
+    std::unordered_map<handler_t, commands_queue_t> m_streams;
     handler_t m_last{std::numeric_limits<handler_t>::max()};
 };
 
@@ -76,43 +133,31 @@ void receive(handler_t h, commands_t commands, size_type num_of_commands)
         cmd_stream << commands[cntr] << '\n';
 
     auto& cmd_collector{el->second};
-    auto print = [h, &cmd_collector]()
-    {
-        std::stringstream fname;
-        fname << h << "-bulk" << cmd_collector.block_start_time(0) << '-' << cmd_collector.block_id() << ".log";
-        std::fstream file{fname.str(), std::fstream::out | std::fstream::app};
-        file << "bulk: ";
-        std::cout << "bulk: ";
-
-        std::size_t cntr = 0;
-        for(const auto& cmd:cmd_collector.get_cmd())
-        {
-            file << cmd;
-            std::cout << cmd;
-            if(++cntr < cmd_collector.block_size())
-            {
-                file << ", ";
-                std::cout << ", ";
-            }
-
-        }
-        std::cout << '\n';
-        file << '\n';
-        cmd_collector.clear_commands();
-    };
-
-    auto process = [&cmd_collector, &print](std::string&& read)
+    auto process = [&cmd_collector](std::string&& read)
     {
         cmd_collector.process_cmd(std::move(read));
         read.clear();
-        if(cmd_collector.input_block_finished())
-            print();
     };
 
     read_input<decltype(process), CmdCollector::ParseErr>(cmd_stream, std::cerr, process);
-    cmd_collector.finish_block();
-    if(cmd_collector.input_block_finished())
-        print();
+
+    auto process_cmd_queue = [](commands_queue_t& q)
+    {
+        ;
+    };
+
+    std::thread t1{process_cmd_queue};
+    std::thread t2{process_cmd_queue};
+    //for(auto& q:cmd_collector.get_cmd())
+    //{
+    //    for(auto& cmd:q)
+    //    {
+    //        // print
+    //        // write to file
+    //    }
+    //}
+    t1.detach();
+    t2.detach();
 }
 
 }
