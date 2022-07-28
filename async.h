3c699ed635d9477fd38f1f4af9d5f8fa6dceead4
Igor Dedkovskiy <divmy@yandex.ru>
2022-07-28 16:02:27 +0300

    Add comments, update readme

--- async.h
+++ async.h
@@ -12,7 +12,7 @@ using size_type = std::size_t;
 using handler_t = void*;
 
 /// \brief   Create a new context
-/// \arg \a  bulk_size is a size of a bulk (block of commands)
+/// \arg \a  bulk_size is a size of a block of commands
 /// \returns a handler to context
 handler_t connect(size_type bulk_size);
 
@@ -21,6 +21,9 @@ int disconnect(handler_t h);
 /// \brief Transfer commands to a context
 void receive(handler_t h, const char* data, size_type data_size);
 
+/// \brief Block current thread until library has finished processing a data
+/// \note  If you need access to an output files, then you need to wait
+///        until files are closed by a library.
 void wait();
 
 }
